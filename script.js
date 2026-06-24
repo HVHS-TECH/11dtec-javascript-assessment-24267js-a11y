8851b3df0592dc23a168209d1c0af9f00f913407
24267js-a11y <24267js@hvhs.school.nz>
2026-06-24 03:41:16 +0000

    Jun 24, 2026, 3:41 AM

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ console.log("Hello world!")
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
 var products = {
-    YujaAde:
-    {amount: 0, price: 0}
+    yujaade: { amount: 0, _price: 0}
+    strawberrylemonade: {amount: 0, _price: 0}
 }
 function displayProducts(_product,_price){
 OUTPUT.innerHTML += +_product +_price ;
