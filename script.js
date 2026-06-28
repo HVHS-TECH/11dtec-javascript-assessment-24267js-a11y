1a90402f6055afd37685047bdfdaa617b2685579
24267js-a11y <24267js@hvhs.school.nz>
2026-06-28 21:50:03 +0000

    Jun 28, 2026, 9:50 PM

--- script.js
+++ script.js
@@ -19,16 +19,6 @@ var products = {
     "yuja ade": { amount: 0, _price: 0},
     "strawberry,lemon ade": { amount: 0, _price: 0 },
     "Korean Strawberry Milk": { amount: 0, _price: 0 },
-    "Yuja Tea (Hot)": { amount: 0, _price: 0 },
-    "Ginger Honey Tea (Hot)": { amount: 0, _price: 0 },
-    "Soboro bread": { amount: 0, _price: 0 },
-    "Injeolmi Cream Bread": { amount: 0, _price: 0 },
-    "Korean Garlic Cream": { amount: 0, _price: 0 },
-     "Gochujang Bread:": { amount: 0, _price: 0 },
-     "Strawberry Cheesecake:": { amount: 0, _price: 0 },
-     "Red Velvet Cupcakes:": { amount: 0, _price: 0 },
-     "Ham & Cheese Croissant:": { amount: 0, _price: 0 },
-     "Spinach & Feta Roll:": { amount: 0, _price: 0 },
      finalnumber: 0,
 };
 
