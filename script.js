73bc18176390d0aa71df2ca8c3d5fbda0f8bdf98
24267js-a11y <24267js@hvhs.school.nz>
2026-07-05 00:05:08 +0000

    Jul 5, 2026, 12:05 AM

--- script.js
+++ script.js
@@ -30,6 +30,27 @@ function itemlist() {
 }
 
 
+function getYJInput() {
+    let YJInput = document.getElementById("YJField").value;
+    products["yuja ade"].amount = YJInput;
+    products["yuja ade"]._price = YJInput * 5.00;
+    OUTPUT.innerHTML += "<br>Yuja Ade: " + products["yuja ade"].amount + " Price: $" + products["yuja ade"]._price.toFixed(2) + "<br>";
+}
+
+function getSLInput() {
+    let SLInput = document.getElementById("SLField").value;
+    products["strawberry,lemon ade"].amount = SLInput;
+    products["strawberry,lemon ade"]._price = SLInput * 6.00;
+    OUTPUT.innerHTML += "<br>Strawberry Lemon Ade: " + products["strawberry,lemon ade"].amount + " Price: $" + products["strawberry,lemon ade"]._price.toFixed(2) + "<br>";
+}
+
+function getKSMInput() {
+    let KSMInput = document.getElementById("KSMField").value;
+    products["Korean Strawberry Milk"].amount = KSMInput;
+    products["Korean Strawberry Milk"]._price = KSMInput * 7.00;
+    OUTPUT.innerHTML += "<br>Korean Strawberry Milk: " + products["Korean Strawberry Milk"].amount + " Price: $" + products["Korean Strawberry Milk"]._price.toFixed(2) + "<br>";
+}
+
 function ViewItems() {
     getFormInput();
     itemlist();
