42736621f664436819cde7a99e1a1b7f6e530e6b
24267js-a11y <24267js@hvhs.school.nz>
2026-07-13 23:35:25 +0000

    Jul 13, 2026, 11:35 PM

--- script.js
+++ script.js
@@ -85,14 +85,7 @@ function moneyField() {
     OUTPUT.innerHTML += "<p>You have $" + money + " to spend.</p>";
 }
 function ViewItems() {
-   if (itemArray.length > 0) {
-        OUTPUT.innerHTML += "<p>Items in cart:</p>";
-        for (let i = 0; i < itemArray.length; i++) {
-            OUTPUT.innerHTML += "<p>" + itemArray[i] + "</p>";
-        }
-    } else {
-        OUTPUT.innerHTML += "<p>No items in cart.</p>";
-    }
+  
    
 }
 
