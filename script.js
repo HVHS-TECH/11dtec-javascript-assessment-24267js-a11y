128100919a5b83b881cbb275e83d94459c264315
24267js-a11y <24267js@hvhs.school.nz>
2026-07-20 22:34:43 +0000

    sfdffdf

--- script.js
+++ script.js
@@ -57,6 +57,23 @@ function calculateTotal() {
 
 
 
+
+
+function getNameInput() {
+    const NAME_FIELD = document.getElementById("nameField");
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+    let name = NAME_FIELD.value;
+    OUTPUT.innerHTML += "<p>Hello, " + name + "!</p>";
+    
+}
+
+function getMoneyInput() {
+    const MONEY_FIELD = document.getElementById("moneyField");
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+    let money = MONEY_FIELD.value;
+    OUTPUT.innerHTML += "<p>You have $" + money + " to spend.</p>";
+}
+
 function worth() {
     const MONEY_FIELD = document.getElementById("moneyField");
     const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
@@ -69,20 +86,6 @@ function worth() {
     }
 
     return money;
-}
-
-function nameField() {
-    const NAME_FIELD = document.getElementById("nameField");
-    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-    let name = NAME_FIELD.value;
-    OUTPUT.innerHTML += "<p>Hello, " + name + "!</p>";
-}
-
-function moneyField() {
-    const MONEY_FIELD = document.getElementById("moneyField");
-    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-    let money = MONEY_FIELD.value;
-    OUTPUT.innerHTML += "<p>You have $" + money + " to spend.</p>";
 }
 
 
