6457f31314f5efc430fce9aa4331c12703808871
24267js-a11y <24267js@hvhs.school.nz>
2026-07-24 05:42:07 +0000

    Jul 24, 2026, 5:42 AM

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ let itemArray = [
 function ItemList() {
      OUTPUT.innerHTML += "<br>These Are The Items Availabile <br>";
     for(i=0; i<itemArray; i++){
-        OUTPUT.innerHTML += i + ":" + itemArray.name[i] + "<br>" 
+        OUTPUT.innerHTML += i + ":" + itemArray[i].name + "<br>" 
 }
 }
 
